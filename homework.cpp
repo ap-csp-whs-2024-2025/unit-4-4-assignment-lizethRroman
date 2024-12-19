3d461740863ffebca527fde8b2b6b2b273ae626c
lizethRroman <lroman101@student.hbuhsd.edu>
2024-12-19 21:43:40 +0000

    math

--- homework.cpp
+++ homework.cpp
@@ -6,11 +6,25 @@ int main()
     // Complete the homework problems here
     
     // Problem 1
-    
-    
+    int num;
+    std::cout << "Enter a number from 1-100";
+    cin >> num;
+
+    while (number < 1 || number > 100);
+    std::cout "That number in invalid"
+    cout >> "Enter a new number"
+    std::cin >> num;
+
     // Problem 2
+    std::stringFavColor = "pink";
+    string guess;
+    int = 1
+
+    std::cout << "Guess what my favorite color is";
     
-    
+
+
+
     // Problem 3
     
     
